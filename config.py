d86c2d7e716803b502874a82ca80ce7cbd7e4054
Orv <70858122+ItsOrv@users.noreply.github.com>
2024-10-18 18:07:28 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = "7340354420:AAEYfHdDZpjb_J9ZOG8OUj70cB4rdNJJPG0"  # توکن ربات
-ADMIN_ID = 6663338157  # ایدی عددی ادمین اصلی
-PAYMENT_ID = "@pouria_orv"  # آیدی ادمین برای تایید پرداخت
+TOKEN = ""  # توکن ربات
+ADMIN_ID =   # ایدی عددی ادمین اصلی
+PAYMENT_ID = ""  # آیدی ادمین برای تایید پرداخت
 DATABASE = "data.json"  # مسیر فایل ذخیره اطلاعات
